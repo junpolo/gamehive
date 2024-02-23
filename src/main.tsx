f3157aefc1d69486e3272fda506693a512311fbb
Jun Polo <junpolojr@gmail.com>
2024-02-23 15:50:19 +0800

    fix: incorrect import

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import { HelmetProvider } from "react-helmet-async";
 import { BrowserRouter } from "react-router-dom";
 import { CookiesProvider } from "react-cookie";
 
-import App from "@/App";
+import App from "./App";
 import "./index.scss";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
