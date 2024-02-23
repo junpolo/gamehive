5937690e48b1110df48ca5f75303bdfcaa9bafc1
Alfranciss W. Dionsay <alfranciss@hyperstacksinc.com>
2024-02-23 13:59:26 +0800

    feat: auth default

--- src/scenes/Auth/auth.scene.tsx
+++ src/scenes/Auth/auth.scene.tsx
@@ -1,13 +1,5 @@
-import GameList from "@/@core/component/game-list.component";
-import { Box, Typography } from "@mui/material";
-
+import { Typography } from "@mui/material";
 const EntryScene = () => {
-  return (
-    <Box>
-      <Typography>Auth Scene</Typography>
-      <GameList />
-    </Box>
-  );
+  return <Typography>Auth Scene</Typography>;
 };
-
 export default EntryScene;
