80aad4ee59e500fda0915e0c4400f2d0f934ed5e
Jun Polo <junpolojr@gmail.com>
2024-02-26 16:56:26 +0800

    fix(dashboard): incorrect layout

--- src/scenes/Dashboard/dashboard.scene.tsx
+++ src/scenes/Dashboard/dashboard.scene.tsx
@@ -5,20 +5,14 @@ import GameListScene from "./components/scenes/game-list-scene";
 
 const DashboardScene = () => {
   return (
-    <Grid
-      container
-      justifyContent={"center"}
-      spacing={{ xs: 5, md: 10 }}
-      p={5}
-      pt={{ xs: 13 }}
-    >
-      <Grid item md={5} xs={12}>
-        <Stack spacing={{ xs: 5, md: 2 }}>
+    <Grid container spacing={4}>
+      <Grid item xs={12} md={8}>
+        <Stack spacing={4}>
           <FeatureGames />
           <GameListScene />
         </Stack>
       </Grid>
-      <Grid item md={3} xs={12}>
+      <Grid item xs={12} md={4}>
         <Leaderboard />
       </Grid>
     </Grid>
